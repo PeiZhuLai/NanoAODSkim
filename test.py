84067cfd355d5163a36d4edba379ba06bad2a6a5
Pei-Zhu Lai <ht910155@gmail.com>
2025-06-25 14:19:49 +0200

    Update test.py

--- test.py
+++ test.py
@@ -1,5 +1,6 @@
 import uproot
 
+# file = uproot.open("root://cms-xrd-global.cern.ch//store/mc/RunIISummer20UL16NanoAODAPVv9/WW_TuneCP5_13TeV-pythia8/NANOAODSIM/106X_mcRun2_asymptotic_preVFP_v11-v1/130000/802F2B91-4B57-2141-93AB-BCA13F3D593E.root")
 file = uproot.open("root://cms-xrd-global.cern.ch//store/mc/RunIISummer20UL16NanoAODAPVv9/WW_TuneCP5_13TeV-pythia8/NANOAODSIM/106X_mcRun2_asymptotic_preVFP_v11-v1/130000/802F2B91-4B57-2141-93AB-BCA13F3D593E.root")
 tree = file["Events"]
 branches = tree.keys()
